4f7fa6a41957905667a08b474f47898962a789e2
Tadeo Bennett <tadeos.bennett@gmail.com>
2024-04-06 03:37:29 -0600

    update report 1

--- job_portal/includes/users.inc.php
+++ job_portal/includes/users.inc.php
@@ -8,6 +8,7 @@ if(isset($_POST["registerAccount"])){ //if a button to add a new user is clicked
         $newEmployer = new Employer(0, $_POST["firstname"], $_POST["lastname"], $_POST["password"], $_POST["repeatedPassword"], $_POST["companyEmail"], $_POST["companyName"], $_POST["phoneNumber"]);
         $controller = new APIController();
         $controller->registerUser($newEmployer);
+        
     }else if(isset($_POST["firstname"])){
     }
 }
